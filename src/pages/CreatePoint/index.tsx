309199a1605e98887e61a5939591718396f2ae95
Henrique Araújo <henriquearaujoo@gmail.com>
2020-06-07 22:29:13 -0400

    Upload de arquivos

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -7,6 +7,7 @@ import logo from '../../assets/logo.svg';
 import { FiArrowLeft } from 'react-icons/fi';
 import api from '../../services/api';
 import axios from 'axios';
+import Dropzone from '../../components/Dropzone';
 
 interface Item {
   id: number;
@@ -44,6 +45,7 @@ const CreatePoint = () => {
     email: '',
     whatsapp: '',
   });
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((posittion) => {
@@ -129,16 +131,19 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
 
     await api.post('/points', data);
 
@@ -162,6 +167,7 @@ const CreatePoint = () => {
           Cadastro do
           <br /> ponto de coleta
         </h1>
+        <Dropzone onFileUploaded={setSelectedFile} />
         <fieldset>
           <legend>
             <h2>Dados</h2>
